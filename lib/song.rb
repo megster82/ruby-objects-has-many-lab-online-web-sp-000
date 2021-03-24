fbef28443f3e4ea7d6d4dd590057a90cf7b4a0b1
meaghan jennings <meaghanljennings@gmail.com>
2021-03-24 22:48:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,19 @@
 class Song
+  attr_accessor :artist, :name
+  @@all = []
   
+  def initialize(name)
+    @name = name
+  end
   
+  def self.all
+    @@all
+  end 
   
-  
-  
-  
-  
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil 
+    end
 end
